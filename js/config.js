6e1d0ba423ab1a5d5610cd04fae418d4a9cb45d3
Alexandros Kavroulakis <akavroulakis@gmail.com>
2022-07-13 07:24:17 +0100

    updates Cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_4iywRwpOV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5f0odkfdhvu54n45gal7rdb0j9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
